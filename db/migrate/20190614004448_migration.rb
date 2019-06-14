a54c38f5ed4c446954cfb9b09bf81d14dd595988
tomoko ichikawa <takator@yahoo.co.jp>
2019-06-14 11:11:51 +0900

    20190614_2

--- db/migrate/20190614004448_migration.rb
+++
@@ -1,5 +0,0 @@
-class Migration < ActiveRecord::Migration[5.1]
-  def change
-      add_colum :name, :string
-  end
-end
--- db/migrate/20190614020105_add_name_to_users.rb
+++ db/migrate/20190614020105_add_name_to_users.rb
@@ -1,5 +1,5 @@
 class AddNameToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :Name, :string
+      add_column :name, :string
   end
 end
